2d6c7fb32e5ea3315745f67f1320e491c57a8317
Lara Sanders <larasanders@gmail.com>
2018-01-09 00:41:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,3 @@ class ApplicationController < Sinatra::Base
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
 end
-
-get '/' do
-  erb :index
-end
